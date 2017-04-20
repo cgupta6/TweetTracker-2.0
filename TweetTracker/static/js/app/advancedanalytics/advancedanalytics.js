8e8b6732450cbc3653abfcdd75308a1e7dbea706
Philippe Faucon <christophe@asu.edu>
2017-04-20 11:18:36 -0700

    replace MockUI with symbolic links to relevant directories
    Works fine on my system, will push to git, test, and then merge.

--- TweetTracker/static/js/app/advancedanalytics/advancedanalytics.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by anjoy92 on 4/14/17.
- */
-
-
-app.controller('advancedAnalyticsCtrl', function ( $scope, $location, $http ,$rootScope,dynamicHeader) {
-    dynamicHeader.setReportTab($location.$$path);
-});
--- TweetTracker/static/js/app/home/home.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by anjoy92 on 4/14/17.
- */
-
-app.controller('homeCtrl', function ( $scope, $location, $http ,$rootScope,dynamicHeader) {
-    dynamicHeader.setReportTab($location.$$path);
-});
